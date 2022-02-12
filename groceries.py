b42c18281d044c8b672e0b04a2251f1f581062e4
rewayod <97519521+rewayod@users.noreply.github.com>
2022-02-11 19:44:32 -0500

    Update groceries.py

--- groceries.py
+++ groceries.py
@@ -1,4 +1,4 @@
-products = [
+products = []
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
     {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
